e657f7dead7a902ff9146e706ba50cc31e240b60
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2024-12-02 21:54:42 -0300

    [✨] [feat](DefaultController.java): Remove unused imports from DefaultController class.

--- src/main/java/io/ordini/products/adapter/controller/DefaultController.java
+++ src/main/java/io/ordini/products/adapter/controller/DefaultController.java
@@ -1,8 +1,8 @@
 package io.ordini.products.adapter.controller;
 
-import io.ordini.products.application.files.FileService;
-import io.ordini.products.domain.repository.IProcessedFileRepository;
-import io.ordini.products.infrastructure.batch.ProductsBatchConfiguration;
+//import io.ordini.products.application.files.FileService;
+//import io.ordini.products.domain.repository.IProcessedFileRepository;
+//import io.ordini.products.infrastructure.batch.ProductsBatchConfiguration;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+//import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 @AllArgsConstructor
 public class DefaultController {
 
-  private FileService fileService;
-  private IProcessedFileRepository processedFileRepository;
-  private ProductsBatchConfiguration productsBatchConfiguration;
+//  private FileService fileService;
+//  private IProcessedFileRepository processedFileRepository;
+//  private ProductsBatchConfiguration productsBatchConfiguration;
 
   private record BodyResponse(String status) {
   }
@@ -31,13 +31,13 @@ public class DefaultController {
     return ResponseEntity.status(HttpStatus.OK).body(new BodyResponse("ok"));
   }
 
-  @GetMapping("/bucket/objects")
-  @Operation(summary = "List all objects in the bucket")
-  public ResponseEntity<Map<String, Map<String, Object>> > listObjectsInBucket() {
-
-    Map<String, Map<String, Object>> fileContents = fileService.getFilesAndContent();
-    productsBatchConfiguration.runJobsForAllFiles();
-
-    return ResponseEntity.status(200).body(fileContents);
-  }
+//  @GetMapping("/bucket/objects")
+//  @Operation(summary = "List all objects in the bucket")
+//  public ResponseEntity<Map<String, Map<String, Object>> > listObjectsInBucket() {
+//
+//    Map<String, Map<String, Object>> fileContents = fileService.getFilesAndContent();
+//    productsBatchConfiguration.runJobsForAllFiles();
+//
+//    return ResponseEntity.status(200).body(fileContents);
+//  }
 }
